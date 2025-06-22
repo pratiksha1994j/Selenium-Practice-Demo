0bc8adef14279287d0cf3ddab2e8d576bdec168c
unknown <pratiksha1994j@gmail.com>
2025-06-22 13:26:11 +0530

    added new file

---
+++ selinium_practice_demo/src/main/java/com/example/StackDemo.java
@@ -0,0 +1,13 @@
+package com.example;
+import java.util.*;
+public class StackDemo {
+    
+    public static void main(String args[]){
+
+        Stack <Integer> st =new Stack<>();
+          st.push(20);
+
+          System.out.println(st);
+    }
+}
+
